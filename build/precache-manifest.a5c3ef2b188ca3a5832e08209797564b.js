2a59335dc07222249700f372bf116d551ff6aad1
Ankit Chaurasia <ankitchaurasia750@gmail.com>
2022-12-22 17:49:18 +0530

    first commit

---
+++ build/precache-manifest.a5c3ef2b188ca3a5832e08209797564b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d2f704c66d96abbfd40d05cc646580c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df6908ee5d08540a1465",
+    "url": "/static/css/main.b40b865c.chunk.css"
+  },
+  {
+    "revision": "3aa1c17e79335c860e7f",
+    "url": "/static/js/2.a7fa318e.chunk.js"
+  },
+  {
+    "revision": "bf744d37492ffed4d3003a8f244c622c",
+    "url": "/static/js/2.a7fa318e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6908ee5d08540a1465",
+    "url": "/static/js/main.f26f281f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "bc31eff31581ea06326d8b785b9507e0",
+    "url": "/static/media/logo1.bc31eff3.png"
+  }
+]);
